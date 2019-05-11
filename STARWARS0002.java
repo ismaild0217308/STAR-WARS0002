62b1e24acda4001ca5973ed60e37ae5bc6437959
ISMAIL UMAR <43749027+ismaild0217308@users.noreply.github.com>
2019-05-11 11:41:48 +0800

    Update STARWARS0002.java

--- STARWARS0002.java
+++ STARWARS0002.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 //NAMA : ISMAIL UMAR
 //NIM  : D0217308
 //KELAS: INFORMATIKA. A 2017
-public class STARWARS0002 {	
+
+public class STARWARS0002 {
+
 	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
+
 		System.out.println("INPUT");
 		System.out.print("panjang : ");
 		double panjang = input.nextDouble();
 		System.out.print("lebar : ");
 		double lebar = input.nextDouble();
-	
-
-		double luas = ( panjang * lebar );
-		double keliling = 2 * (panjang + lebar);
 		
+		double luas = (panjang * lebar);
+		double keliling = 2 * (panjang + lebar);
+
 		System.out.println("OUTPUT");
 		System.out.println("luas : " + luas);
-		System.out.println("keliling : " + keliling);
-		}
+		System.out.println("keliling: " + keliling);
 	}
-
+}
